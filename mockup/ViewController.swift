c54e16e5fbd83383d921b7a03313857ff3f0e4dd
Ryan Stich <55109747+RyanStich@users.noreply.github.com>
2021-03-08 15:14:54 -0800

    :airplane:

---
+++ mockup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prototype
+//
+//  Created by Boss Hog on 2021-03-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
